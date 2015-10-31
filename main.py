f6eeef3222d450c6a9651560c1103c0804b73a44
Matthew Grogan <mvgnyc@gmail.com>
2015-10-31 15:29:26 -0400

    Updated color scheme

--- main.py
+++ main.py
@@ -3,32 +3,26 @@ from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.properties import StringProperty, ObjectProperty
+from kivy.utils import get_color_from_hex
 from problems import TwoDigitAddition
 
 class MentalMathLayout(BoxLayout):
     
     response = StringProperty(None)
     response_label = ObjectProperty(None)
+    problem = StringProperty(None)
    
-    problem = TwoDigitAddition()
-    a = StringProperty("")
-    b = StringProperty("")
-    answer = StringProperty("")
-    operator = StringProperty("")
-    
-    def _generate(self):
-        self.problem.generate()
-        self.a = str(self.problem.a)
-        self.b = str(self.problem.b)
-        self.answer = str(self.problem.answer)
-        self.operator = self.problem.operator
+    def generate(self):
+        pg = TwoDigitAddition()
+        self.problem = "%s\n%s %s" % (pg.a, pg.operator, pg.b)
+        self.answer = str(pg.answer)
 
     def clear(self):
         self.response = ""
         
     def next(self, *args):
         self.clear()
-        self._generate()
+        self.generate()
         
     def enter(self):
         # Check the response
@@ -39,13 +33,13 @@ class MentalMathLayout(BoxLayout):
         
     def correct(self):
         # Show the response
-        correct_str = "Correct! %s %s %s = %s" % (self.a, self.operator, self.b, self.answer)
+        correct_str = "Correct!"
         self.response = correct_str
         
         Clock.schedule_once(self.next, 1)
         
     def incorrect(self):
-        incorrect_str = "%s is incorrect. %s %s %s = %s" % (self.response, self.a, self.operator, self.b, self.answer)
+        incorrect_str = "Incorrect"
         self.response = incorrect_str
         
         Clock.schedule_once(self.next, 1)
